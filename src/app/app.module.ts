7733100cfc3a3859cc882e4018353d1f7195e749
ariantefrancesco@gmail.com <Kekko1128>
2022-12-02 12:10:16 +0100

    add firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// Firestore section
+import { environment } from 'src/environments/environment';
+import { provideFirebaseApp, getApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +17,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAl_RAI9D3X-f0DBWqOuXLnLQkuqClPAeA",
+    authDomain: "esempiocustomer.firebaseapp.com",
+    projectId: "esempiocustomer",
+    storageBucket: "esempiocustomer.appspot.com",
+    messagingSenderId: "208587941864",
+    appId: "1:208587941864:web:b4100bb324905f2a2ba65d",
+    measurementId: "G-8S1SED8WS8"
+  }
+}
